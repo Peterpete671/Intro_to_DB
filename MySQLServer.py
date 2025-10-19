4a2967a7eaafba2e4ff2b91e7afc9f43ffe16338
Peter Mwaura <mwaurapeter1202@gmail.com>
2025-10-19 16:32:12 +0300

    Add MySQLServer.py script to create 'alx_book_store' database with proper error handling

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
 	try:
@@ -14,13 +13,13 @@ def create_database():
 			cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 			print("Database 'alx_book_store' created successfully!")
 
-	except Error as e:
-		print(f"Error while connecting to MySQL or creating database: {e}")
+	except mysql.connector.Error as err:
+		print(f"Error while connecting to MySQL or creating database: {err}")
 
 	finally:
-		#Safely closing connection
-		if connection.is_connected():
+		if cursor:
 			cursor.close()
+		if connection and connection.is_connected():
 			connection.close()
 
 if __name__ == "__main__":
